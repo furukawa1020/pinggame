ce7e3b542331bac9a531ad5f29c49537bc780c5f
furukawa1020 <f.kotaro.0530@gmail.com>
2025-10-05 16:17:21 +0900

    feat: GameSceneコンポーネントを新規作成し、基本的な3Dオブジェクトを追加

---
+++ src/components/3D/GameScene.tsx
@@ -0,0 +1,34 @@
+﻿import React, { useRef, useState, useEffect, useCallback } from 'react';
+import { useFrame } from '@react-three/fiber';
+import * as THREE from 'three';
+
+const GameScene: React.FC = () => {
+  return (
+    <>
+      <ambientLight intensity={0.6} />
+      <directionalLight position={[10, 10, 5]} intensity={1} />
+      
+      <mesh position={[0, -1, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+        <planeGeometry args={[20, 20]} />
+        <meshStandardMaterial color="#F0E68C" />
+      </mesh>
+      
+      <mesh position={[0, 0, 0]}>
+        <sphereGeometry args={[0.5, 16, 16]} />
+        <meshStandardMaterial color="#2C3E50" />
+      </mesh>
+      
+      <mesh position={[2, 1, 2]}>
+        <boxGeometry args={[1.0, 0.6, 0.3]} />
+        <meshStandardMaterial color="#FFA07A" />
+      </mesh>
+      
+      <mesh position={[-2, 1, -2]}>
+        <sphereGeometry args={[0.4, 16, 16]} />
+        <meshStandardMaterial color="#FF69B4" />
+      </mesh>
+    </>
+  );
+};
+
+export default GameScene;
